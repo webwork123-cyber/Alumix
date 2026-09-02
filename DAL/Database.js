17f273a003598fd36bc883e25b71771649e1888e
eliegnasr05-oss <https://github.com/eliegnasr05-oss/Alumix.git>
2026-09-02 21:42:06 +0300

    initial

--- DAL/Database.js
+++ DAL/Database.js
@@ -1,12 +1,17 @@
-const mySql=require("mysql2/promise");
+const mysql = require("mysql2/promise");
 
-const pool=mySql.createPool({
-    host:"localhost",
-    user:"root",
-    password:"Elie2005",
-    database:"alumix_db",
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT) || 3306,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
-(async()=> {
+
+(async () => {
     try {
         const connection = await pool.getConnection();
         console.log("Database connected successfully!");
@@ -16,4 +21,5 @@ const pool=mySql.createPool({
         process.exit(1);
     }
 })();
-module.exports=pool;
+
+module.exports = pool;
